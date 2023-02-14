8d813dc085311a55eaffa41cc369c04364eff087
Shravan Aras <123.shravan@gmail.com>
2023-02-13 21:35:06 -0700

    Added requirements and setup file.

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+# A quick script that sets things up.
+
+mkdir logs_predict logs_training
+mkdir logs_predict/graphs logs_predict/loss
+mkdir logs_training/graphs logs_training/loss
